e09b9514f63661a0c4fc6a804cde589ad5a88afb
Twil3akine <twil3akine@gmail.com>
2025-01-10 22:35:44 +0900

    first commit

---
+++ userscript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         atcoder-check-ac
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  check and show AC
+// @author       twil3akine
+// @match        https://atcoder.jp/contests/*/tasks/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
